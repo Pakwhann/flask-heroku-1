907be943c029def57d0a425cdf07963f1ddaf21f
Pakwhann <87840019+Pakwhann@users.noreply.github.com>
2021-08-20 09:21:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-        return "ณภัทร ตรีรัตน์ฤดี เลขที่ 12 ม.4/10"
+        return "ณภัทร ตรีรัตน์ฤดี <br>เลขที่ 12 ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
